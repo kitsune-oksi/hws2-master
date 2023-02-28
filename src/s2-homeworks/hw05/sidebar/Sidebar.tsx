4b237c7dcd7f06b7997fed820dff1db3940355b9
OksanaN <nurova.oksana2020@gmail.com>
2023-02-28 14:38:30 +0500

    HW5
    * 1 - write routes to all pages in the Pages.tsx file
    * 2 - write className in the Sidebar.tsx file so that the s.active class is hung
    * 3 - styling

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -9,9 +9,10 @@ type PropsType = {
     handleClose: () => void
 }
 
+const activeLinkStyle = ({isActive}: {isActive: boolean}) => isActive ? s.active : ''
+
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
-    const sidebarClass = s.sidebar
-        + (open ? ' ' + s.open : '')
+    const sidebarClass = `${s.sidebar} ${open ? s.open : ''}`
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={activeLinkStyle} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={activeLinkStyle} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +48,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={activeLinkStyle} // делает студент
                     >
                         Junior Plus
                     </NavLink>
