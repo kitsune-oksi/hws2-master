44cb447b9489bedda2765a39b4fd8141969aa01b
OksanaN <nurova.oksana2020@gmail.com>
2023-05-04 12:44:41 +0500

    * 1 - add types and logic (sort by name, filter by age) homeWorkReducer, check with a test * 2 - append to the User component * 3 - make styles according to design

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,23 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === 'up') {
+                let copyState = [...state];
+                return copyState.sort(
+                    (a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1)
+            }
+            if (action.payload === 'down') {
+                let copyState = [...state];
+                return copyState.sort(
+                    (a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1)
+            }
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= 18) // need to fix
         }
         default:
             return state
